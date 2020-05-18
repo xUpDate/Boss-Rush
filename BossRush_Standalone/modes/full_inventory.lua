460f8a05be8e71f354ca4f45dd1cf30b6d4f2215
xUpDate <49315393+xUpDate@users.noreply.github.com>
2020-05-18 14:16:55 +0200

    Update full_inventory.lua

--- BossRush_Standalone/modes/full_inventory.lua
+++ BossRush_Standalone/modes/full_inventory.lua
@@ -2,19 +2,19 @@ function bottles()
 	if boss_rush == true then
 		if modes.full_inventory == true then
 				if boss_rush_items == false then
-						addFlag(0x02002B32, 0x55) -- Items
-						addFlag(0x02002B33, 0x51)
-						addFlag(0x02002B34, 0x55)	
-						addFlag(0x02002B35, 0x55)
-						addFlag(0x02002B36, 0x54)
-						addFlag(0x02002B37, 0x45)
-						addFlag(0x02002B39, 0x55)
-						addFlag(0x02002B39, 0x55) -- Bottles
-						addFlag(0x02002B43, 0x55) -- Grip Ring / Power Bracelets / Flippers / Map of Hyrule
-						addFlag(0x02002B44, 0x55) -- Scrolls: Spin Attack / Roll Attack / Dash Attack / Rock Breaker
-						addFlag(0x02002B45, 0x55) -- Scrolls: Sword Beam / Great Spin Attack / Downthrust / Peril Beam
-						addFlag(0x02002B4E, 0x55) -- Butterflies + Fast Spin Charge
-						addFlag(0x02002B4F, 0x05) -- Split Gauge / Longer Spin Attack
+						mwb(0x02002B32, 0x55) -- Items
+						mwb(0x02002B33, 0x51)
+						mwb(0x02002B34, 0x55)	
+						mwb(0x02002B35, 0x55)
+						mwb(0x02002B36, 0x54)
+						mwb(0x02002B37, 0x45)
+						mwb(0x02002B39, 0x55)
+						mwb(0x02002B39, 0x55) -- Bottles
+						mwb(0x02002B43, 0x55) -- Grip Ring / Power Bracelets / Flippers / Map of Hyrule
+						mwb(0x02002B44, 0x55) -- Scrolls: Spin Attack / Roll Attack / Dash Attack / Rock Breaker
+						mwb(0x02002B45, 0x55) -- Scrolls: Sword Beam / Great Spin Attack / Downthrust / Peril Beam
+						mwb(0x02002B4E, 0x55) -- Butterflies + Fast Spin Charge
+						mwb(0x02002B4F, 0x05) -- Split Gauge / Longer Spin Attack
 						mwb(0x02002AEE, 0x3) -- Big Bomb Bag
 						mwb(0x02002AEF, 0x3)
 						addFlag(0x02002B4B, 0x10) -- Big Quiver
